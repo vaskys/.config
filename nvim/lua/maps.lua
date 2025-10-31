efdd9433f798a71cd14072c7ad7df63276d95aca
Samuel Vasky <cotiajviemmladyxD@eksdi.com>
2025-10-31 22:57:41 +0100

    ds

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -8,12 +8,8 @@ local function map_nowait(mode, lhs, rhs)
   vim.keymap.set(mode, lhs, rhs, { silent = true, noremap = true, nowait = true })
 end
 
-
-map_nowait("n", "-", "$")
-
 map("n", "J", "<Nop>")
 map("n", "K", "<Nop>")
-
 
 map("n", "dd", [["_dd]])
 map("n", "D",  [["_D]])
@@ -22,21 +18,19 @@ map({ "n", "v", "x", "o" }, "c",    [["_c]])
 map({ "n", "v", "x", "o" }, "<Del>",[["_d]])
 
 -- Window Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
+map("n", "<leader>h", "<C-w>h")
+map("n", "<leader>l", "<C-w>l")
+map("n", "<leader>k", "<C-w>k")
+map("n", "<leader>j", "<C-w>j")
 
-
-map("n", "<C-e>", "<CMD>vs<CR>")
-map("n", "<C-w>", "<CMD>close<CR>")
-map("n", "E", "<CMD>vs<CR>")
-map("n", "W", "<CMD>close<CR>")
+map("n", "<leader>e", "<CMD>vs<CR>")
+map("n", "<leader>w", "<CMD>close<CR>")
 
 -- Telescope
-map("n", "<C-p>", "<CMD>Telescope live_grep<CR>")
-map("n", "P", ":Telescope find_files<CR>")
-map_nowait("n", "ff", ":Spectre<CR>")
+map("n", "<leader>g", "<CMD>Telescope live_grep<CR>")
+map("n", "<leader><leader>", ":Telescope find_files<CR>")
+map("n", "<leader>c", ":Telescope commands<CR>")
+map_nowait("n", "<leader>ff", ":Spectre<CR>")
 
 -- LSP
 map("n", "gd", "<CMD>lua vim.lsp.buf.definition()<CR>")
@@ -55,10 +49,7 @@ map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>")
 map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>")
 map("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>")
 map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>")
-map(
-  "n",
-  "<Leader>dd",
-  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+map("n","<Leader>dd",  "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>")
 map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>")
 
