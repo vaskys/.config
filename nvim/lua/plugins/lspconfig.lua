cc417acc9aa12cd77f6688d53a1a310d750447d8
Samuel Vasky <cotiajviemmladyxD@eksdi.com>
2025-11-08 21:08:19 +0100

    c

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,20 +1,46 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        { "folke/neodev.nvim", opts = {} },
-         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-    },
-    config = function()
-        local mason_lspconfig = require("mason-lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        vim.lsp.enable({
-              "clangd",
-              "sourcekit",
-              "pyright",
-              "stylua"
-        })
-    end,
+  "hrsh7th/cmp-nvim-lsp",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/lazydev.nvim", opts = {} },
+  },
+  config = function()
+    -- import cmp-nvim-lsp plugin
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
+     -- Explicitly enable snippet support and placeholders
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+      properties = { "documentation", "detail", "additionalTextEdits" },
+    }
+
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.config.clangd = {
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--offset-encoding=utf-8",
+        "--completion-style=detailed",
+        "--function-arg-placeholders=true",
+        "--header-insertion=never"
+      },
+      root_markers = { ".clangd", "compile_commands.json", ".git" },
+      filetypes = { "c", "cpp", "objc", "objcpp" },
+    }
+    vim.lsp.enable({
+        "clangd",
+        "sourcekit",
+        "pyright",
+    })
+  end,
 }
--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,22 +1,53 @@
 return {
-    "williamboman/mason.nvim",
+  {
+    "https://github.com/jay-babu/mason-nvim-dap.nvim",
     dependencies = {
-        "williamboman/mason-lspconfig.nvim",
+      "williamboman/mason.nvim",
+      "https://github.com/mfussenegger/nvim-dap"
     },
-    config = function()
-        require("mason").setup()
-
-        require("mason-lspconfig").setup({
-            automatic_installation = true,
-            ensure_installed = {
-                "cssls",
-                "eslint",
-                "html",
-                "jsonls",
-                "pyright",
-                "tailwindcss",
-                "clangd",
+    opts = {
+      handles = {},
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      -- list of servers for mason to install
+      ensure_installed = {
+        "pyright",
+        "clangd"
+      },
+    },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
             },
-        })
-    end,
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+    },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
 }
